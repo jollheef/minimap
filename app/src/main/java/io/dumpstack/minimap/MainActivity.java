b5abd01dd2492cd57f38c7b111aa04b22e34bb38
Mikhail Klementev <blame@dumpstack.io>
2020-02-23 20:24:57 +0000

    Add map (OSM)

--- app/src/main/java/io/dumpstack/minimap/MainActivity.java
+++ app/src/main/java/io/dumpstack/minimap/MainActivity.java
@@ -2,19 +2,33 @@ package io.dumpstack.minimap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
 
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.MapView;
+
 public class MainActivity extends AppCompatActivity implements GestureDetector.OnGestureListener {
 
-    GestureDetector gestureDetector;
+    private GestureDetector gestureDetector;
+    private MapView map;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+
         setContentView(R.layout.activity_main);
+
+        map = findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
@@ -24,6 +38,16 @@ public class MainActivity extends AppCompatActivity implements GestureDetector.O
         decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
 
         this.gestureDetector = new GestureDetector(this, this);
+    }
+
+    public void onResume(){
+        super.onResume();
+        map.onResume();
+    }
+
+    public void onPause(){
+        super.onPause();
+        map.onPause();
     }
 
     @Override
